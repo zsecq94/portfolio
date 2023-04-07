e872fbf4a38d596dbe7ef94e0f54a539f9fc712b
배중권 <zsecq2@naver.com>
2023-04-07 17:35:53 +0900

    230407 2

---
+++ ptfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Header from "./layout/Header";
+import VerticalSlider from "./pages/VerticalSlider";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <VerticalSlider />
+    </div>
+  );
+}
+
+export default App;
---
+++ ptfolio/src/layout/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.scss";
+
+const header = () => {
+  return (
+    <div className="header-wrapper">
+      <img src="/images/react.png" className="App-logo1" alt="logo" />
+      <img src="/images/vuejs.png" className="App-logo2" alt="logo" />
+      <img src="/images/python.png" className="App-logo3" alt="logo" />
+      <img src="/images/js.png" className="App-logo4" alt="logo" />
+    </div>
+  );
+};
+
+export default header;
