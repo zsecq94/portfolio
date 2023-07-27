7864a8e8d33077da09c0c184f5f5fe9ff048308b
zsecq94 <zsecq94@gmail.com>
2023-07-27 19:43:38 +0900

    230727

--- new/client/src/pages/About/index.jsx
+++ new/client/src/pages/About/index.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { FaBirthdayCake } from "react-icons/fa";
-import { MdPlace, MdAttachEmail } from "react-icons/md";
-import { BsFillPersonFill, BsPenFill } from "react-icons/bs";
-import { RiContactsBook2Fill } from "react-icons/ri";
 
 import "./About.scss";
 
@@ -21,53 +17,20 @@ const About = () => {
         <h2>✔ ABOUT ME</h2>
       </div>
       <div className="about-wrapper">
-        <div className="about1">
-          <div className="name">
-            <BsFillPersonFill size={"2rem"} />
-            <div>
-              <h3>이름</h3>
-              <h3>배중권</h3>
-            </div>
-          </div>
-          <div className="age">
-            <FaBirthdayCake size={"2rem"} />
-            <div>
-              <h3>생년월일</h3>
-              <h3>94.10.03</h3>
-            </div>
-          </div>
-          <div className="address">
-            <MdPlace size={"2rem"} />
-            <div>
-              <h3>주소지</h3>
-              <h3>전라남도 나주시</h3>
-            </div>
-          </div>
+        <div className="about-title">
+          <h2>About Me</h2>
+          <h4>https://jk-zone.firebaseapp.com/</h4>
         </div>
-        <div className="about2">
-          <div className="contact">
-            <RiContactsBook2Fill size={"2rem"} />
-            <div>
-              <h3>연락처</h3>
-              <h3>010-2556-8761</h3>
-            </div>
-          </div>
-          <div className="email">
-            <MdAttachEmail size={"2rem"} />
-            <div>
-              <h3>이메일</h3>
-              <h3>zsecq94@gmail.com</h3>
-            </div>
-          </div>
-          <div className="edu">
-            <BsPenFill size={"2rem"} />
-            <div>
-              <h3>교육</h3>
-              <h3>동신대학교 졸업</h3>
-              <h3>삼성 청년 SW아카데미 수료</h3>
-            </div>
-          </div>
+        <div className="about-main">
+          <h4>프론트엔드 개발자</h4>
+          <h3>배 중 권 / 94.10.03</h3>
+          <h4>zsecq94@gmail.com / 010-2556-8761 / 전라남도 나주시</h4>
+          <h4>삼성 청년 SW 아카데미 8기 수료</h4>
         </div>
+      </div>
+      <div className="about-wrapper2">
+        <h2>JK-ZONE</h2>
+        <h3>사용자 친화적 / 디테일 / 열정</h3>
       </div>
     </Container>
   );
@@ -82,71 +45,46 @@ const Container = styled.div`
   background-color: #aee2ff;
   width: 100%;
   height: 100%;
-  gap: 1rem;
+  gap: 2rem;
   color: white;
   .about-wrapper {
     display: flex;
     flex-direction: column;
     color: black;
     background-color: white;
-    width: 40%;
+    background-image: url("images/Business.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 30%;
     height: 30%;
-    border-radius: 20px;
-    .about1 {
+    box-shadow: 3px 3px 3px;
+    padding: 20px 40px;
+    justify-content: space-between;
+    h4 {
+      font-size: 1rem;
+    }
+    .about-title {
       display: flex;
       justify-content: space-between;
       align-items: center;
-      height: 50%;
-      padding: 20px;
-      .name {
-        width: 33%;
-        justify-content: center;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-      }
-      .age {
-        width: 33%;
-        justify-content: center;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-      }
-      .address {
-        width: 33%;
-        justify-content: center;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-      }
     }
-    .about2 {
+    .about-main {
       display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: 50%;
-      padding: 20px;
-      .contact {
-        width: 33%;
-        justify-content: center;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-      }
-      .email {
-        width: 33%;
-        justify-content: center;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-      }
-      .edu {
-        width: 33%;
-        justify-content: center;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-      }
+      flex-direction: column;
+      text-align: end;
     }
+  }
+  .about-wrapper2 {
+    display: flex;
+    flex-direction: column;
+    color: black;
+    background-color: white;
+    width: 30%;
+    height: 30%;
+    box-shadow: 3px 3px 3px;
+    padding: 20px 40px;
+    justify-content: center;
+    align-items: center;
   }
 `;
